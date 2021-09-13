088439fb6b1a3c2e1891b95d5baace0ac8d8c4fe
twoosky <lhjae3@naver.com>
2021-09-13 20:52:07 +0900

    Create project

---
+++ src/main/java/hanuel/springbootnotepad/SpringBootNotepadApplication.java
@@ -0,0 +1,13 @@
+package hanuel.springbootnotepad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNotepadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootNotepadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hanuel/springbootnotepad/SpringBootNotepadApplicationTests.java
@@ -0,0 +1,13 @@
+package hanuel.springbootnotepad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootNotepadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
